cac941d43e34aa275963a29bfd0e7bf4df33ddd0
Sponge-bink <spongebink@gmail.com>
2019-01-23 13:36:56 +0800

    Init commit.

--- Safari View Controller demo/ViewController.swift
+++ Safari View Controller demo/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func watchButtonTapped(_ sender: UIButton) {
+        watchVideo()
+    }
+    
+    func watchVideo() {
+        showSafariVC(for: "https://www.youtube.com/watch?v=hMapDBmEwpE")
+    }
+    func showSafariVC(for url: String) {
+        guard let url = URL(string: url) else {
+            return
+        }
+        let safariVC = SFSafariViewController(url: url)
+        present(safariVC, animated: true)
+    }
 }
 
